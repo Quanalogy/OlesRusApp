d1981521e9995704f5190d6ea4b9a312bb508fb6
Borimino <markusandreassen1994@gmail.com>
2014-09-11 14:23:49 +0200

    Added method to be added on button-click

--- app/src/main/java/oles/rus/app/olesrusapp/MyActivity.java
+++ app/src/main/java/oles/rus/app/olesrusapp/MyActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class MyActivity extends Activity {
@@ -28,9 +29,11 @@ public class MyActivity extends Activity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+        return id == R.id.action_settings || super.onOptionsItemSelected(item);
+    }
+
+    public void sendStudyNumber(View view)
+    {
+
     }
 }
